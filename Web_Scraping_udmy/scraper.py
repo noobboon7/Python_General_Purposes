3b2457eb0a8205461d77b93f119545438f121d03
Jerry V <jvr572@gmail.com>
2020-12-31 19:14:42 -0500

    working on scraping for content

---
+++ Web_Scraping_udmy/scraper.py
@@ -0,0 +1,6 @@
+import requests
+import bs4 
+
+result = requests.get("https://en.wikipedia.org/w/index.php?title=Wikipedia&banner=S2021_1123_enUS_dsk_lg_control&force=1&country=US&utm_campaign=C2021_social&utm_medium=google&utm_source=enUS2021_fr_search_brand_wikipedia_WPArticlePage_var2_text")
+
+# print(result.text)
